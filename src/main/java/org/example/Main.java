5048872f15209ea1d242851f978bb5f9ddfb56c4
Rasul Iskandarov <73297277+rasul-isk@users.noreply.github.com>
2023-03-24 12:10:14 +0200

    added file reader method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.io.*;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    static ArrayList<String> ReadFile(String fileName) {
+        try {
+            InputStream input = Main.class.getClassLoader().getResourceAsStream("Game Data/" + fileName);
+
+            if (input == null) {
+                throw new FileNotFoundException("File not found: " + fileName);
+            }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            ArrayList<String> lines = new ArrayList<String>();
+            String line;
+            while ((line =  reader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            reader.close();
+
+            return lines;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (String line : ReadFile("game_data_0.txt")){
+            System.out.println(line);
+        }
     }
 }
